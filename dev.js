668876a38f686243ca0247b4b5245aeba84ca8d7
cemalpoguzntt <101316757+cemalpoguzntt@users.noreply.github.com>
2023-05-10 15:09:05 +0300

    Squashed commit of the following:
    commit 9a1106c92b467b6e301912b7207698c32e9c5384
    Merge: 1668e76 93edc12
    Author: cemalpoguzntt <101316757+cemalpoguzntt@users.noreply.github.com>
    Date:   Wed May 10 15:02:35 2023 +0300
    
        Merge branch 'DEV' of github.com:cemalpoguzntt/for-launch-beta into DEV
    
    commit 1668e763035913144498bc755b8afc755a0ea96d
    Author: cemalpoguzntt <101316757+cemalpoguzntt@users.noreply.github.com>
    Date:   Wed May 10 15:00:03 2023 +0300
    
        conflict deneme
    
    commit 93edc124345d4634c7aa75b17243e468ae0a48f9
    Merge: 6645c64 cbcba71
    Author: cemalpoguzntt <101316757+cemalpoguzntt@users.noreply.github.com>
    Date:   Wed May 10 14:50:41 2023 +0300
    
        Merge pull request #1 from cemalpoguzntt:dev-issue-2
    
        Fix

--- dev.js
+++ dev.js
@@ -1,4 +1,7 @@
 console.log("first")
 var asd = "",
-    tayyib ='sa'
-    aaa = "nesdasdw line"
+    tayyib = "sa"
+aaa = "nesdasdw line"
+var asd2 = "sswww",
+    cemiko = 12312,
+    aa2 = "new lineasasdd"
